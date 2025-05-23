c80a563674c331f6bce0d710f3ea04eedd0d631f
Ailton dos Santos <ailton.santos@web.de>
2025-05-23 14:28:20 -0300

    Image Upload
    Initial commit - projeto Django com formulário de imagem

---
+++ 03_AULA 03/Upload/produtos/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+class ProdutoForm(forms.Form):
+    nome = forms.CharField(label='Nome do Produto', max_length=100)
+    descricao = forms.CharField(widget=forms.Textarea, label='Descrição')
+    preco = forms.DecimalField(label='Preço', max_digits=10, decimal_places=2)
+    estoque = forms.IntegerField(label='Quantidade em Estoque')
+    categoria = forms.ChoiceField(
+        label='Categoria',
+        choices=[
+            ('Eletrônico', 'Eletrônico'),
+            ('Eletrodoméstico', 'Eletrodoméstico'),
+            ('Móvel', 'Móvel'),
+            ('Outro', 'Outro')
+        ]
+    )
+    validade = forms.DateField(label='Data de Validade', widget=forms.DateInput(attrs={'type': 'date'}))
+    imagem = forms.ImageField(label='Imagem do Produto', required=False)
---
+++ 03_AULA 03/Upload/produtos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import ProdutoForm
+
+def cadastrar_produto(request):
+    mensagem = ''
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, request.FILES)
+        if form.is_valid():
+            dados = form.cleaned_data
+            mensagem = f"Produto '{dados['nome']}' cadastrado com sucesso!"
+    else:
+        form = ProdutoForm()
+
+    return render(request, 'produtos/cadastro.html', {'form': form, 'mensagem': mensagem})
