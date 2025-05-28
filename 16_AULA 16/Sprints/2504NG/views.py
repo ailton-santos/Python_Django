1ecbd2ccd6d0d7cceb9acfee7d0c2710d7a3a61a
Beatriz Silva <143009734+beatriz-ssilva@users.noreply.github.com>
2025-05-28 19:02:53 -0300

    Exemplo 3

---
+++ 16_AULA 16/Sprints/2504NG/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .forms import InfoForm
+
+def enviar_info(request): #request confirma se o formulario esta sendo enviado
+    mensagem = ''
+
+    if request.method == 'POST':  #POST
+        form = InfoForm(request.POST)
+        if form.is_valid():
+            nomeProduto = form.cleaned_data['nomeProduto']
+            descricao = form.cleaned_data['descricao']
+            preco = form.cleaned_data['preco']
+            quantidadeEstoque = form.cleaned_data['quantidadeEstoque']
+            categoria = form.cleaned_data['categoria']
+            mensagem = f"Cadastro efetuado com sucesso! Produto cadastrado: {nomeProduto} | {descricao} | R$ {preco} reais | {quantidadeEstoque} unidades | {categoria}"
+    else:
+        form = InfoForm() #coloquei os parenteses, se der erro tira q tava certo antes
+    return render (request, 'formulario/formulario.html', {'form' : form, 'mensagem' : mensagem})
+
+
